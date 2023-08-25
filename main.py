1f3da35e79bafd806c4d70e73eefacf0e469b226
Yuriy Kuchma <krabatua@gmail.com>
2023-08-25 14:06:07 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from datetime import date, datetime
+
+
+def get_birthdays_per_week(users):
+    # Реалізуйте тут домашнє завдання
+    return users
+
+
+if __name__ == "__main__":
+    users = [
+        {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
+    ]
+
+    result = get_birthdays_per_week(users)
+    print(result)
+    # Виводимо результат
+    for day_name, names in result.items():
+        print(f"{day_name}: {', '.join(names)}")
