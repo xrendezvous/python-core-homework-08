c4721f56c43da8417293da838d1b5ce46712f53a
Aryna Reutska <arina.reutskaya110@gmail.com>
2023-11-06 19:59:23 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
 from datetime import date, datetime, timedelta
-
-
 def get_birthdays_per_week(users):
-    now = datetime.today().date()
-    end_of_week = now + timedelta(days=(4 - now.weekday() + 7) % 7)
-    weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-    result = {day: [] for day in weekdays}
-
+    if not users:
+        return {}
+    current_date = date.today()
+    weekdays = {
+        0: 'Monday',
+        1: 'Tuesday',
+        2: 'Wednesday',
+        3: 'Thursday',
+        4: 'Friday',
+        5: 'Saturday',
+        6: 'Sunday'
+    }
+    birthdays_per_week = {day: [] for day in weekdays.values()}
     for user in users:
-        user_birthday = user.get('birthday').replace(year=now.year).date()
-        if now <= user_birthday <= end_of_week:
-            day_name = weekdays[user_birthday.weekday()]
-            user_name = user.get('name').split()[0]
-            result[day_name].append(user_name)
-        elif user_birthday.year > now.year and user_birthday <= end_of_week:
-            day_name = weekdays[user_birthday.weekday()]
-            user_name = user.get('name').split()[0]
-            result[day_name].append(user_name)
-
-    return result
+        name = user['name']
+        birthday = user['birthday']
+        next_birthday = birthday.replace(year=current_date.year)
+        if next_birthday < current_date:
+            next_birthday = next_birthday.replace(year=current_date.year + 1)
+        # Перевіряємо, чи дата народження потрапляє в наступний тиждень
+        if current_date <= next_birthday <= current_date + timedelta(days=7):
+            day_of_week = next_birthday.weekday()
+            day_name = weekdays[day_of_week]  # Отримуємо назву дня тижня
+            # Перевірка, чи день народження падає на вихідний
+            if day_name in ['Saturday', 'Sunday']:
+                day_name = 'Monday'  # Переносимо на понеділок
+            birthdays_per_week[day_name].append(name)
+    if not any(birthdays_per_week.values()):
+        return {}
+    print(birthdays_per_week)
+    return birthdays_per_week
 
 
 if __name__ == "__main__":
     users = [
         {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
+        {"name": "Masha", "birthday": date(2023, 11, 9)},
+        {"name": "Olya", "birthday": date(2023, 11, 3)},
+        {"name": "Kolya", "birthday": date(2023, 11, 4)},
+        {"name": "Sophia", "birthday": date(2023, 11, 5)},
+        {"name": "Solomia", "birthday": date(2023, 11, 6)},
+        {"name": "Sasha", "birthday": date(2023, 11, 7)},
+        {"name": "Pasha", "birthday": date(2023, 11, 8)},
+        {"name": "Oksana", "birthday": date(2023, 11, 10)}
     ]
 
     result = get_birthdays_per_week(users)
